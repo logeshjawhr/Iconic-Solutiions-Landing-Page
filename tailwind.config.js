25f8408677e7a6411130fba26b369f2709ea939b
Logesh S R <logeshjawhr@gmail.com>
2024-03-08 21:47:41 +0530

    Design:header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "navcolor":"#EEEEEE",
+        "bg":"#FFD369"
+      }
+    },
   },
   plugins: [],
 }
